67180ac3abc7ebf38bd037d1f7a63a108667db80
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-08 14:34:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,15 +8,10 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+  
     alpha = 0.02
     counts = np.array([x_success, y_success])
     nobs = np.array([x_cnt, y_cnt])
     stat, pval = proportions_ztest(counts, nobs, alternative = 'larger')
-    if pval < alpha:
-      res = True
-    else:
-      res = False    
-    return res 
+    
+    return pval < alpha 
